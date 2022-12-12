a8e96325e1548fb87849d07c78e803c07d70bd13
Leandro Cantuário <111256173+leandrocantuario@users.noreply.github.com>
2022-12-11 22:53:45 -0300

    Criação de Lâmpada
    Utilização de HTML, CSS e JavaScript

---
+++ lampada.js
@@ -0,0 +1,20 @@
+var lamp = window.document.getElementById('lampada')
+
+function estaQuebrada(){
+    return lamp.src.indexOf('quebrada') > -1
+}
+
+function ligar(){
+    if (!estaQuebrada()){
+    lamp.src = 'ligada.svg'}
+}
+
+function desligar(){
+    if (!estaQuebrada()){
+    lamp.src = 'desligada.svg'}
+}
+
+lamp.addEventListener ('click', quebrar)
+function quebrar(){
+    lamp.src = 'quebrada.svg'
+}
